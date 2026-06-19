c5b92ddedc8378bcaf72f85e8b84a89cf6528e56
Emmanuel Obakpolor <emmanuelobakpolor6655@gmail.com>
2026-06-19 09:40:20 +0100

    DFG

--- src/components/BrandLogo.jsx
+++ src/components/BrandLogo.jsx
@@ -2,10 +2,7 @@ export default function BrandLogo() {
   return (
     <div className="brand-block">
       <img src="/LOGOADMIN.png" alt="Axira" className="brand-logo-img" />
-      <div className="brand-copy">
-        <h1>AXIRA</h1>
-        <p>MANAGEMENT DASHBOARD</p>
-      </div>
+     
     </div>
   );
 }
